fdfeb3f356c8f8c8942861d2e08e2ea59a6b5b55
Jordan Primas <jorprimas20@gmail.com>
2023-01-17 19:44:19 -0800

    commit tree a7bf1fd5c26dd5a5635fe8c67642334931f1ca12

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const gifts = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(gifts) {
+    const event = "birthday"
+    const message = []
+    for (let i = 0; i < gifts.length; i++) {
+        message.push(`Thank you, ${gifts[i]}, for the wonderful surprise gift!`) 
+    }
+    return message 
+}
+
+writeCards(gifts)
+
+let integers = 11
+function countDown(integers) {
+    while (integers >= 0) {
+        console.log(integers--)
+}
+return integers 
+}
+countDown(integers)
